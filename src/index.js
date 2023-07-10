18a8f085d5b35d53ee21a8b162bff07bd0b723f7
Bob Oyier <boboyier92@gmail.com>
2023-07-10 22:10:08 +0300

    Complete deliverable 2

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', loadFirstBeer);
 const beerName = document.querySelector('.beer-details h2');
 const beerImage = document.querySelector('.beer-details img');
 const beerDescription = document.querySelector('.beer-details p');
-
+const beersList = document.querySelector('#beer-list');
 const BEERS_URL = 'https://api.npoint.io/e87daf8b5ac4bf34f345/beers';
 
-let beers = [];
+let beers;
 let singleBeer;
 
 function loadFirstBeer() {
@@ -14,13 +14,24 @@ function loadFirstBeer() {
 		.then((res) => res.json())
 		.then((data) => {
 			beers = data;
+			loadAllBeers(beers);
 			renderBeer(beers[0]);
 		});
 }
 
 function renderBeer(singleBeer) {
 	const { name, image_url, description } = singleBeer;
+
 	beerName.textContent = name;
 	beerImage.src = image_url;
 	beerDescription.textContent = description;
+}
+
+function loadAllBeers(beers) {
+	beers.map((beer) => {
+		const singleBeer = document.createElement('li');
+
+		singleBeer.textContent = beer.name;
+		beersList.appendChild(singleBeer);
+	});
 }
